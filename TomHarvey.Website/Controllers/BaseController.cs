c6568eea1cb28c0981ec01cbd63f2b7dc21b7398
Tom <tom@ibuildstuff.co.uk>
2011-10-14 22:16:23 +0100

    ditched the reference on .admin and started work on a website template

---
+++ TomHarvey.Website/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+using System.Web.Mvc;
+
+namespace TomHarvey.Website.Controllers
+{
+    public class BaseController : Controller
+    {
+        // TODO: selected menu item
+    }
+}
---
+++ TomHarvey.Website/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace TomHarvey.Website.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public ViewResult Index()
+        {
+            return View("Template");
+        }
+    }
+}
