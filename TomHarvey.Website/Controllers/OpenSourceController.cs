8295d57bb7152123259cffa87aa44afbd6485001
Tom <tom@ibuildstuff.co.uk>
2011-10-23 21:58:10 +0100

    added in the open source project detail stuff

--- TomHarvey.Website/Controllers/OpenSourceController.cs
+++ TomHarvey.Website/Controllers/OpenSourceController.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Web.Mvc;
+using TomHarvey.Admin.Business.Interfaces;
+using TomHarvey.Website.Models.OpenSource;
+using WeBuildStuff.PageManagement.Business.Interfaces;
 
 namespace TomHarvey.Website.Controllers
 {
     public class OpenSourceController : BaseController
     {
+        private readonly IPageDetailsRepository _pageDetailsRepository;
+        private readonly IPageRevisionsRepository _pageRevisionsRepository;
+        private readonly IOpenSourceProjectDetailsRepository _openSourceProjectDetailsRepository;
+        private readonly IOpenSourceProjectLinksRepository _openSourceProjectLinksRepository;
+
+        public OpenSourceController(IPageDetailsRepository pageDetailsRepository,
+                                    IPageRevisionsRepository pageRevisionsRepository,
+                                    IOpenSourceProjectDetailsRepository openSourceProjectDetailsRepository,
+                                    IOpenSourceProjectLinksRepository openSourceProjectLinksRepository)
+        {
+            _pageDetailsRepository = pageDetailsRepository;
+            _pageRevisionsRepository = pageRevisionsRepository;
+            _openSourceProjectDetailsRepository = openSourceProjectDetailsRepository;
+            _openSourceProjectLinksRepository = openSourceProjectLinksRepository;
+        }
+
         public ViewResult Index()
         {
             throw new NotImplementedException();
         }
 
-        // https://github.com/tombuildsstuff/repositories
+        public ActionResult Details(string id)
+        {
+            var project = _openSourceProjectDetailsRepository.GetByUrl(id);
+            if (project == null)
+                return new HttpNotFoundResult();
+
+            var links = _openSourceProjectLinksRepository.GetAllForProject(project.Id);
+            return View("details", new OpenSourceProjectDetails(project, links /* images? */));
+        }
+
+        // https://github.com/tombuildsstuff/repositories (use in content; with extra project details available)
     }
 }
---
+++ TomHarvey.Website/Models/OpenSource/OpenSourceProjectDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using TomHarvey.Admin.Business.OpenSource;
+
+namespace TomHarvey.Website.Models.OpenSource
+{
+    public class OpenSourceProjectDetails
+    {
+        public OpenSourceProjectDetail Project { get; set; }
+        public IEnumerable<OpenSourceProjectLink> Links { get; set; }
+
+        public OpenSourceProjectDetails(OpenSourceProjectDetail project, IEnumerable<OpenSourceProjectLink> links)
+        {
+            Project = project;
+            Links = links;
+        }
+    }
+}
