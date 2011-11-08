bfaad202e8b218488e813a584eed1241050136c3
Tom Harvey <tom@ibuildstuff.co.uk>
2011-11-08 15:49:46 +0000

    moved out the 'load image' function into a seperate method; then reused this in the service, main photo for portfolio and portfolio image items

--- TomHarvey.Website/Controllers/ImagesController.cs
+++ TomHarvey.Website/Controllers/ImagesController.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Web.Mvc;
 using OpenFileSystem.IO;
 using TomHarvey.Admin.Business.Interfaces;
 using TomHarvey.Core.Helpers;
+using WeBuildStuff.Services.Business.Interfaces;
 using WeBuildStuff.Shared.Settings;
 using Path = System.IO.Path;
 
@@ -15,16 +15,22 @@ namespace TomHarvey.Website.Controllers
         private readonly IPortfolioImagesRepository _portfolioImagesRepository;
         private readonly IFileSystem _fileSystem;
         private readonly ISettingsRepository _settingsRepository;
+        private readonly IServiceDetailsRepository _serviceDetailsRepository;
+        private readonly IServicePhotosRepository _servicePhotosRepository;
 
         public ImagesController(IPortfolioItemsRepository portfolioItemsRepository,
                                 IPortfolioImagesRepository portfolioImagesRepository,
                                 IFileSystem fileSystem,
-                                ISettingsRepository settingsRepository)
+                                ISettingsRepository settingsRepository,
+                                IServiceDetailsRepository serviceDetailsRepository,
+                                IServicePhotosRepository servicePhotosRepository)
         {
             _portfolioItemsRepository = portfolioItemsRepository;
             _portfolioImagesRepository = portfolioImagesRepository;
             _fileSystem = fileSystem;
             _settingsRepository = settingsRepository;
+            _serviceDetailsRepository = serviceDetailsRepository;
+            _servicePhotosRepository = servicePhotosRepository;
         }
 
         public ActionResult Portfolio(int id, int additional)
@@ -34,36 +40,59 @@ namespace TomHarvey.Website.Controllers
                 return new HttpNotFoundResult();
 
             var portfolioImage = _portfolioImagesRepository.GetById(additional);
-            if (portfolioImage == null || portfolioImage.Removed)
+            if (portfolioImage == null || portfolioImage.Removed || portfolioImage.PortfolioItemId != id)
                 return new HttpNotFoundResult();
 
-            try
-            {
-                var filePath = Path.Combine(_settingsRepository.StorageBaseDirectory(),
-                                        Path.DirectorySeparatorChar.ToString(),
-                                        portfolioImage.FileName);
-                var file = _fileSystem.GetFile(filePath);
-                byte[] fileContent;
-                using (var fileStream = file.OpenRead())
-                    fileContent = fileStream.ToByteArray();
-
-                return File(fileContent, "image/jpeg", portfolioImage.FileName);
-            }
-            catch (FileNotFoundException)
-            {
+            var bytes = GetImageContents("Portfolio", portfolioImage.FileName);
+            if (bytes == null)
                 return new HttpNotFoundResult();
-            }
+
+            return File(bytes, "image/jpeg", portfolioImage.FileName);
         }
 
         [ActionName("portfolio-main")]
         public ActionResult PortfolioMain(int id)
         {
-            throw new NotImplementedException();
+            var main = _portfolioImagesRepository.GetMainForPortfolioItem(id);
+            return RedirectToActionPermanent("portfolio", new { id, additional = main.Id });
         }
 
         public ActionResult Service(int id, int additional)
         {
-            throw new NotImplementedException();
+            var service = _serviceDetailsRepository.GetById(id);
+            if (service == null || service.Deleted)
+                return new HttpNotFoundResult();
+
+            var photo = _servicePhotosRepository.GetServicePhotoById(additional);
+            if (photo == null || photo.Deleted || photo.ServiceId != id)
+                return new HttpNotFoundResult();
+
+            var bytes = GetImageContents("Services", string.Format("{0}.jpg", photo.Id));
+            if (bytes == null)
+                return new HttpNotFoundResult();
+
+            return File(bytes, "image/jpeg");
+        }
+
+        private byte[] GetImageContents(string folderName, string fileName)
+        {
+            try
+            {
+                var filePath = Path.Combine(_settingsRepository.StorageBaseDirectory(),
+                                            Path.DirectorySeparatorChar.ToString(),
+                                            folderName,
+                                            Path.DirectorySeparatorChar.ToString(),
+                                            fileName);
+                var file = _fileSystem.GetFile(filePath);
+                byte[] fileContent;
+                using (var fileStream = file.OpenRead())
+                    fileContent = fileStream.ToByteArray();
+
+                return fileContent;
+            
+            } catch (FileNotFoundException) {
+                return null;
+            }
         }
     }
 }
